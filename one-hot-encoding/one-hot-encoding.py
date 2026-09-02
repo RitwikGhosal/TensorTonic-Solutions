ac3f7acff736b81d55f7617e722c5b71a78ebde6
Ritwik Ghosal <51014635+RitwikGhosal@users.noreply.github.com>
2026-09-02 16:30:10 +0530

    Solve one-hot-encoding

---
+++ one-hot-encoding/one-hot-encoding.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def one_hot(y: list, num_classes=None) -> np.ndarray:
+    """
+    Returns a NumPy array with shape (N, K).
+    """
+    y = np.asarray(y, dtype = int)
+    if num_classes is None:
+        num_classes = int(np.max(y)) + 1
+    encoded = np.zeros((y.size, num_classes), dtype = float)
+    encoded[np.arange(y.size),y] = 1.0
+    return encoded
