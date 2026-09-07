75387b0616d7d734ae420c4bdf95d5a3f0c2e416
Ritwik Ghosal <51014635+RitwikGhosal@users.noreply.github.com>
2026-09-07 17:19:57 +0530

    Solve lag-features

---
+++ lag-features/lag-features.py
@@ -0,0 +1,11 @@
+def lag_features(series: list, lags: list) -> list:
+    """
+    Returns the lag feature matrix.
+    """
+    max_lag = max(lags)
+    result = []
+    for t in range(max_lag, len(series)):
+        row = [series[t - lag] for lag in lags]
+        result.append(row)
+    return result
+    
